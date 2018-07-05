9f7a6dee43ca76d5ce39f2963660383f9ba57358
bnlong <bnlong@fossil.com>
2018-07-05 11:53:00 +0700

    kong/plugins/zipkin/reporter.lua: convert opentracing tags to zipkin tags.

--- kong/plugins/zipkin/reporter.lua
+++ kong/plugins/zipkin/reporter.lua
@@ -29,6 +29,11 @@ function zipkin_reporter_methods:report(span)
 	local span_context = span.context
 	local span_kind = span:get_tag "span.kind"
 	local port = span:get_tag "peer.port"
+	local zipkin_tags = {}
+	for k, v in span:each_tag() do
+		zipkin_tags[k] = tostring(v)
+	end
+
 	local zipkin_span = {
 		traceId = to_hex(span_context.trace_id);
 		name = span.name;
@@ -46,7 +51,7 @@ function zipkin_reporter_methods:report(span)
 			ipv6 = span:get_tag "peer.ipv6";
 			port = port; -- port is *not* optional
 		} or cjson.null;
-		tags = span.tags; -- XXX: not guaranteed by documented opentracing-lua API
+		tags = zipkin_tags; -- XXX: not guaranteed by documented opentracing-lua API
 		annotations = span.logs -- XXX: not guaranteed by documented opentracing-lua API to be in correct format
 	}
 
