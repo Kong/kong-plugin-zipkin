519d6e75a7f27e0b7ed75d83b3655b567c2defe4
jeremyjpj0916 <31913027+jeremyjpj0916@users.noreply.github.com>
2019-01-03 20:32:32 -0500

    Update opentracing.lua

--- kong/plugins/zipkin/opentracing.lua
+++ kong/plugins/zipkin/opentracing.lua
@@ -36,9 +36,9 @@ end
 -- Utility function to set either ipv4 or ipv6 tags
 -- nginx apis don't have a flag to indicate whether an address is v4 or v6
 local function ip_tag(addr)
-	--check addr not nil for short circuit situations 
+	--check addr nil for short circuit situations 
 	if addr == nil then
-		return "na"
+		return "peer.na"
 	-- use the presence of "." to signal v4 (v6 uses ":")
 	elseif addr:find(".", 1, true) then
 		return "peer.ipv4"
